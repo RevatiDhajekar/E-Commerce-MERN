3759a678c3e3fb3bb2cd7a49090a9c918e653aad
RevatiDhajekar <revadhajekar1@gmail.com>
2024-10-17 19:47:30 +0530

    Header and footer

--- codebase/client/e-commerce/src/App.js
+++ codebase/client/e-commerce/src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
-import Header from "./component/layout/Header.js";
-import {BrowserRouter as Router} from "react-router-dom";
+import "./App.css";
+import Header from "./component/layout/Header/Header.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import webFont from "webfontloader";
+import React, { useEffect } from "react";
+import Footer from "./component/layout/Footer/Footer.js";
+import Home from "./component/Home/Home.js";
 
 function App() {
+  useEffect(() => {
+    webFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []); //called at first rendering
+
   return (
     <Router>
-      <Header/>
+      <Header />
+      <Routes>
+        <Route exact path="/" Component={Home} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ codebase/client/e-commerce/src/component/Home/Home.js
@@ -0,0 +1,37 @@
+import React, { Fragment } from "react";
+import { CgMouse } from "react-icons/cg";
+import './Home.css';
+import Product from './product.js';
+
+const product = {
+    name : "Blue Tshirt",
+    images :[{url : "https://thefoomer.in/cdn/shop/files/jpeg-optimizer_PATP5180.jpg?v=1685610639"}],
+    price:"300",
+    _id:"revati",
+
+}
+
+const Home = () => {
+  return (
+    <Fragment>
+      <div className='banner'>
+        <p>Welcome to ECommerce</p>
+        <h1>FIND AMAZAING PRODUCTS BELOW</h1>
+        <a href="#container">
+          <button>
+            Scroll <CgMouse />
+          </button>
+        </a>
+      </div>
+
+      <h2 className="homeHeading">Featured Products</h2>
+      <div className="container" id="container">
+       <Product product={product}/>
+      
+
+      </div>
+    </Fragment>
+  );
+};
+
+export default Home;
